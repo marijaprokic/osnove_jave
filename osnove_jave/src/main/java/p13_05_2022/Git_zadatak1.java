ec0ac2c3771fac0b60cc7ee88acae8e6feba42fd
Marija Prokic <marijaprokic@yahoo.com>
2022-05-13 20:35:01 +0200

    Predavanja 13.05 Git Zadatak 1

---
+++ osnove_jave/src/main/java/p13_05_2022/Git_zadatak1.java
@@ -0,0 +1,10 @@
+package p13_05_2022;
+
+public class Git_zadatak1 {
+    public static void main(String[] args) {
+        System.out.println("git add");
+        System.out.println("git restore");
+        System.out.println("git commit -m");
+        System.out.println("git push");
+    }
+}
