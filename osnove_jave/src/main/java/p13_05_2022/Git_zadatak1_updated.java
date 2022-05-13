977c86fc9f67f5e0d77510016224e3e0a1027146
Marija Prokic <marijaprokic@yahoo.com>
2022-05-13 21:16:46 +0200

    Update file name

--- osnove_jave/src/main/java/p13_05_2022/Git_zadatak1_updated.java
+++ osnove_jave/src/main/java/p13_05_2022/Git_zadatak1_updated.java
@@ -1,6 +1,6 @@
 package p13_05_2022;
 
-public class Git_zadatak1 {
+public class Git_zadatak1_updated {
     public static void main(String[] args) {
         System.out.println("git add");
         System.out.println("git restore");
