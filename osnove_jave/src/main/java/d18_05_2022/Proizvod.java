38b6d2d458dbf127305a6225b0a9cab09266bc67
Marija Prokic <marijaprokic@yahoo.com>
2022-05-20 17:26:24 +0200

    Domaći 18.05.2022. Proizvod

---
+++ osnove_jave/src/main/java/d18_05_2022/Proizvod.java
@@ -0,0 +1,17 @@
+package d18_05_2022;
+
+//Napisati klasu Proizvod koja ima atribute
+//        naziv proizvoda (String)
+//        cenu proizvoda (double)
+//        težinu proizvoda u gramima. (double)
+//        i metode:
+//        stampaj - stampa podatke proizvoda u formatu {{naziv}}, {{cena}}, {{tezina}}
+//        U glavnom programu kreirati minimum 2 proizvoda i za svaki od njih pozvati metodu za stampu.
+
+
+public class Proizvod {
+    public String naziv;
+    public double cena;
+    public double tezina;
+
+}
