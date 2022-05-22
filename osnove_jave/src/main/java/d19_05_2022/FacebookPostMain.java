525c142c656722af0a578e401457d67cc68e8d44
Marija Prokic <marijaprokic@yahoo.com>
2022-05-22 20:37:17 +0200

    Domaći 19.05.2022. FacebookPostMain

---
+++ osnove_jave/src/main/java/d19_05_2022/FacebookPostMain.java
@@ -0,0 +1,55 @@
+package d19_05_2022;
+
+import d20_05_2022.FacebookPost;
+
+public class FacebookPostMain {
+    public static void main(String[] args) {
+
+        FacebookPost objava1 = new FacebookPost();
+        objava1.imePrezimeKorisnika = "Marija Prokić";
+        objava1.imePrezimeKorisnikaObjava = "Duško Dugouško";
+        objava1.tekstObjave = "Trebao sam da skrenem levo od Albukerkija...";
+        objava1.brojLajkova = 100;
+        objava1.brojDeljenja = 200;
+
+        FacebookPost objava2 = new FacebookPost();
+        objava2.imePrezimeKorisnika = "Marija Prokić";
+        objava2.imePrezimeKorisnikaObjava = "Elmer lovac";
+        objava2.tekstObjave = "Da li da ga upucam sad, ili kad dodjem kući?";
+        objava2.brojLajkova = 1000;
+        objava2.brojDeljenja = 2000;
+
+        int brojLajkova1_1 = objava1.like();
+        int brojLajkova1_2 = objava1.like();
+        int brojLajkova1_3 = objava1.like();
+        int brojLajkova1_4 = objava1.like();
+        System.out.println("Broj lajkova za objavu 1 je sada: " + brojLajkova1_4);
+
+        int brojLajkovaPosleDislajka1_1 = objava1.dislike();
+        int brojLajkovaPosleDislajka1_2 = objava1.dislike();
+        System.out.println("Broj lajkova za objavu 1 posle dislajkova je: " + brojLajkovaPosleDislajka1_2);
+
+        int brojDeljenja1_1 = objava1.share();
+        int brojDeljenja1_2 = objava1.share();
+        System.out.println("Broj deljenja za objavu 1 je sada: " + brojDeljenja1_2);
+        System.out.println();
+        objava1.print();
+
+        System.out.println();
+
+
+        int brojLajkova2_1 = objava2.like();
+        System.out.println("Broj lajkova za objavu 2 je sada: " + brojLajkova2_1);
+
+        int brojLajkovaPosleDislajka2_1 = objava2.dislike();
+        int brojLajkovaPosleDislajka2_2 = objava2.dislike();
+        System.out.println("Broj lajkova za objavu 2 posle dislajkova je: " + brojLajkovaPosleDislajka2_2);
+
+        int brojDeljenja2_1 = objava2.share();
+        System.out.println("Broj deljenja za objavu 2 je sada: " + brojDeljenja2_1);
+        System.out.println();
+        objava2.print();
+    }
+
+
+}
