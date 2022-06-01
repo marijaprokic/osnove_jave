379fffc8ae5d96bf73c591b15dc7c100834eb435
Marija Prokic <marijaprokic@yahoo.com>
2022-06-01 19:24:48 +0200

    Domaći 30.05.2022. IspitStudentMain prepravka

--- osnove_jave/src/main/java/d30_05_2022/IspitStudentMain.java
+++ osnove_jave/src/main/java/d30_05_2022/IspitStudentMain.java
@@ -5,18 +5,20 @@ public class IspitStudentMain {
     public static void main(String[] args) {
 
 
-    Ispit statika = new Ispit("Statika", 10, "Patak Dača");
-    Ispit matemaika = new Ispit("Matematika", 9, "Elmer lovac");
-    Ispit istorijaArhitekture = new Ispit("Istorija arhitekture", 10, "Tom i Dzeri");
-    Student marija = new Student(1111, "Marija Prokić", "master");
+        Ispit statika = new Ispit("Statika", 10, "Patak Dača");
+        Ispit matemaika = new Ispit("Matematika", 9, "Elmer lovac");
+        Ispit istorijaArhitekture = new Ispit("Istorija arhitekture", 10, "Tom i Dzeri");
+        Ispit mehanika = new Ispit("Mehanika", 5, "Miki maus");
+        Student marija = new Student(1111, "Marija Prokić", "master");
 
-    marija.dodajIspit(statika);
-    marija.dodajIspit(matemaika);
-    marija.dodajIspit(istorijaArhitekture);
+        marija.dodajIspit(statika);
+        marija.dodajIspit(matemaika);
+        marija.dodajIspit(istorijaArhitekture);
+        marija.dodajIspit(mehanika);
 
-    marija.racunajProsek();
+        marija.racunajProsek();
 
-    marija.stampaj();
+        marija.stampaj();
 
 
     }
